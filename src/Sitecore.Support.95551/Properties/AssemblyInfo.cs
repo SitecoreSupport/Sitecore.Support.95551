b10ee33b0b83ec6989c7fe2ac8ce872089afd06b
ido <ido@WS-IDO1UA1>
2017-05-08 21:49:57 +0300

    Compatible with XP 8.2.2

--- src/Sitecore.Support.95551/Properties/AssemblyInfo.cs
+++ src/Sitecore.Support.95551/Properties/AssemblyInfo.cs
@@ -4,3 +4,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Sitecore.Support.95551")]
 [assembly: AssemblyProduct("Sitecore.Support.95551")]
 [assembly: ComVisible(false)]
+[assembly: AssemblyDescription("[UploadMediaDialog] Impossible to change destination folder after files for upoad are selected")]
+
